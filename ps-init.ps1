9e36fb75b3ddac2b9598d2e9201ad2343729de83
Gregory Petri <gcpetri@outlook.com>
2022-01-29 20:03:29 -0600

    bin updates

--- ps-init.ps1
+++ ps-init.ps1
@@ -1,7 +1,5 @@
-cd ../
 ./mvnw clean package -DskipTests
-cp target/rekkme-backend-0.0.1-SNAPSHOT.jar bin/
-cd bin
+cp target/rekkme-backend-0.0.1-SNAPSHOT.jar .
 docker-compose down
 docker rmi rekkme-backend:latest
 docker-compose up
--- unix-init.sh
+++ unix-init.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd ../
 ./mvnw clean package -DskipTests
-cp target/bulletin-0.0.1-SNAPSHOT.jar bin/
-cd bin
+cp target/bulletin-0.0.1-SNAPSHOT.jar .
 docker-compose down
 docker rmi bulletin:latest
 docker-compose up
